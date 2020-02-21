0f0004edb2657c8e63a404af495f79a68cdc1c79
palomalo <malo.pavol@gmail.com>
2020-02-21 17:59:23 +0100

    Unnecessary code removed

--- demo.py
+++ demo.py
@@ -22,67 +22,6 @@ templates = Jinja2Templates(directory=os.path.join(ROOT, "templates"))
 app = Starlette()
 
 
-@app.route("/")
-async def homepage(request):
-    """
-    Simple homepage.
-    """
-    await request.send_push_promise("/style.css")
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
-@app.route("/echo", methods=["POST"])
-async def echo(request):
-    """
-    HTTP echo endpoint.
-    """
-    content = await request.body()
-    media_type = request.headers.get("content-type")
-    return Response(content, media_type=media_type)
-
-
-@app.route("/logs/?")
-async def logs(request):
-    """
-    Browsable list of QLOG files.
-    """
-    logs = []
-    for name in os.listdir(LOGS_PATH):
-        if name.endswith(".qlog"):
-            s = os.stat(os.path.join(LOGS_PATH, name))
-            file_url = "https://" + request.headers["host"] + "/logs/" + name
-            logs.append(
-                {
-                    "date": datetime.datetime.utcfromtimestamp(s.st_mtime).strftime(
-                        "%Y-%m-%d %H:%M:%S"
-                    ),
-                    "file_url": file_url,
-                    "name": name[:-5],
-                    "qvis_url": QVIS_URL
-                    + "?"
-                    + urlencode({"file": file_url})
-                    + "#/sequence",
-                    "size": s.st_size,
-                }
-            )
-    return templates.TemplateResponse(
-        "logs.html",
-        {
-            "logs": sorted(logs, key=lambda x: x["date"], reverse=True),
-            "request": request,
-        },
-    )
-
-
-@app.route("/{size:int}")
-def padding(request):
-    """
-    Dynamically generated data, maximum 50MB.
-    """
-    size = min(50000000, request.path_params["size"])
-    return PlainTextResponse("Z" * size)
-
-
 @app.websocket_route("/ws")
 async def ws(websocket):
     """
